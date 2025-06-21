ba7cc4a7ce97e7f2d5b0de0b8885d8a0ec7c02f7
Phuc Ha Duy Nguyen <phuchaduynguyen@Phucs-MacBook-Air.local>
2025-06-21 14:51:20 +0930

    done

--- part2/app.js
+++ part2/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 require('dotenv').config();
 const session = require('express-session');
 const app = express();
+const db = require('./models/db');
 
 // Middleware
 app.use(express.json());
@@ -21,6 +22,20 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+
+//gettingn all the dogs to display: 
+app.get('/api/dogs', async (req, res) => {
+  try {
+    const [result] = await db.query(
+      `SELECT Dogs.dog_id, Dogs.name, Dogs.size, Dogs.owner_id FROM Dogs;`
+    );
+    res.json(result);
+  } catch {
+    res.status(500).json({ error:'dog data error' });
+  }
+});
+
+
 
 // Export the app instead of listening here
 module.exports = app;
